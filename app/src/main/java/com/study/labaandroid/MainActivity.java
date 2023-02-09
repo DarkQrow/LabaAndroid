1af490006a9d860f6fa775e21816956f94c42ed1
DarkQrow <tultceva13@gmail.com>
2023-02-09 22:39:56 +0300

    rename click functions

--- app/src/main/java/com/study/labaandroid/MainActivity.java
+++ app/src/main/java/com/study/labaandroid/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void GalleryClick(View v){
+    public void galleryClick(View v){
         Intent intent = new Intent(Intent.ACTION_VIEW);
         Uri uri = Uri.parse(directory);
         intent.setDataAndType(uri, "*/*");//specify your type
@@ -23,8 +23,43 @@ public class MainActivity extends AppCompatActivity {
         //startActivity(Intent.createChooser(intent, "Open folder"));
 
     }
-    public void ToolbarClick(View v){
+    public void toolbarClick(View v){
         Intent intent = new Intent(MainActivity.this, ToolBarActivity.class);
         startActivity(intent);
+    }
+    public void photoClick(View v) {
+        Log.d("Photo", "Click!");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_menu_camera)
+                .setContentTitle("NILFULLSCREEN")
+                .setContentText("Photo is made.Coding...")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "PhotoClick";
+            String description = "Photo is codding";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
